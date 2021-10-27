a38de36e70a2041be55ea6b3bc943c6b1743dafd
Muhammad T S <muhammad.ts@neoito.com>
2021-10-27 19:03:41 +0530

    fix: removing x-ms-requires-sync from request

--- pages/api/azureAPI.js
+++ pages/api/azureAPI.js
@@ -9,7 +9,7 @@ const targetBlobName = "Logocopy.png";
 
 const key = process.env.ACCESS_KEY;
 const strTime = new Date().toUTCString();
-const strToSign = `PUT\n\n\n\n\n\n\n\n\n\n\n\nx-ms-access-tier:Cool\nx-ms-copy-source:${blobUrl}\nx-ms-date:${strTime}\nx-ms-rehydrate-priority:High\nx-ms-requires-sync:true\nx-ms-version:2020-10-02\n/${account}/${containerName}/${targetBlobName}\ntimeout:30`;
+const strToSign = `PUT\n\n\n\n\n\n\n\n\n\n\n\nx-ms-access-tier:Cool\nx-ms-copy-source:${blobUrl}\nx-ms-date:${strTime}\nx-ms-rehydrate-priority:High\nx-ms-version:2020-10-02\n/${account}/${containerName}/${targetBlobName}\ntimeout:30`;
 
 const secret = CryptoJS.enc.Base64.parse(key);
 const hash = CryptoJS.HmacSHA256(strToSign, secret);
@@ -25,7 +25,6 @@ const putConfig = {
     "x-ms-copy-source": blobUrl,
     "x-ms-date": strTime,
     "x-ms-rehydrate-priority": "High",
-    "x-ms-requires-sync": "true",
     "x-ms-version": "2020-10-02",
   },
 };
